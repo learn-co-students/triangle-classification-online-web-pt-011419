81aa99c55167ccf3cadf95f714631d4a4c6223b9
A Glazer <avigayilglazer@gmail.com>
2019-02-19 13:42:50 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+
+  end
+
+  def kind
+    if (@s1 + @s2) <= @s3 || (@s2 + @s3) <= @s1 || (@s3 + @s1) <= @s2
+      raise TriangleError
+    end
+
+  end
 end
