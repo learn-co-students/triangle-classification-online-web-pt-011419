a73b88ff16076c7e77d463a8c86c449284dd6895
Ryan Mui <reachryanmui@gmail.com>
2019-03-10 01:17:56 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c, :sides
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @sides = [@a, @b, @c]
+  end
+
+  def kind
+    @sides.each do |length|
+      if length <= 0
+        raise TriangleError
+      elsif (@a + @b) <= @c || (@a +@c) <= @b || (@b + @c) <= @a
+        raise TriangleError
+      end
+    end
+    if @a == @b && @b == @c && @a == @c
+        return :equilateral
+    elsif @a == @b || @b == @c || @a == @c
+        return :isosceles
+    else
+        return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality."
+    end
+  end
+
 end
